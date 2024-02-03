ef5f7b3f379096a14073a251ec5cd9f6fa02b7f1
leel1031 <leel103196@gmail.com>
2024-02-03 11:15:41 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
  */
 const nextConfig = {
     output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
